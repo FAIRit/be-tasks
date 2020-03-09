a1ae33952d37fa8c016bd99bfd54f9d3252b314e
Antonina <amarikhina@gmail.com>
2020-03-09 21:18:18 +0100

    #5 Logging - changed ParentView for UI - for getParent

--- src/test/java/pl/antonina/tasks/parent/ParentMapperTest.java
+++ src/test/java/pl/antonina/tasks/parent/ParentMapperTest.java
@@ -1,6 +1,8 @@
 package pl.antonina.tasks.parent;
 
 import org.junit.jupiter.api.Test;
+import pl.antonina.tasks.user.Gender;
+import pl.antonina.tasks.user.User;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,11 +13,19 @@ class ParentMapperTest {
     @Test
     void mapParentView() {
         final String name = "Antonina";
+        final String email = "test@gmail.com";
+        final Gender parentGender = Gender.FEMALE;
         Parent parent = new Parent();
+        User user = new User();
+        parent.setUser(user);
         parent.setName(name);
+        parent.setGender(parentGender);
+        parent.getUser().setEmail(email);
 
         ParentView parentView = parentMapper.mapParentView(parent);
 
         assertThat(parentView.getName()).isEqualTo(name);
+        assertThat(parentView.getGender()).isEqualTo(parentGender);
+        assertThat(parentView.getEmail()).isEqualTo(email);
     }
 }
